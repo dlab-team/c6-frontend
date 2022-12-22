fb9f80b2457813d69df6dca32ffc94639c64faf5
Javier <j.perezurrutia@gmail.com>
2022-12-22 00:31:20 -0300

    Añadiendo otro ejemplo a archivo DATA-STRUCTURE.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
+import RoutesApp from './routers/Routes';
 
-import { Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
 
-//ejemplo de como importamos distintos componentes
-//como ven sale desde un archivo unico
-import { IndexPage, Home } from './pages/index';
-
-const DefaultLayout = React.lazy(() => import('./routers/DefaultLayout'));
 
 function App() {
   return (
-    <Suspense>
-      <Routes>
-        <Route path='/' element={<DefaultLayout />}>
-          <Route index element={<IndexPage />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <RoutesApp/>
   );
 }
 
---
+++ src/routers/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
+
+//ejemplo de como importamos distintos componentes
+//como ven sale desde un archivo unico
+import { IndexPage, Home } from '../pages/index';
+
+const DefaultLayout = React.lazy(() => import('./DefaultLayout'));
+
+function RoutesApp() {
+  return (
+    <Suspense>
+      <Routes>
+        <Route path='/' element={<DefaultLayout />}>
+          <Route index element={<IndexPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+}
+
+export default RoutesApp;
