df445811224a01ea1ec5e284cfa4dd016db9e390
ramiroguillen <ramirojguillen@gmail.com>
2022-12-23 16:06:37 -0300

    estructura base y contenido de la segunda seccion de la homepage

---
+++ src/pages/Home/components/Section2.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+
+const Section2 = () => {
+    return (
+        <>
+            <section id="como-funcionamos">
+                <div>
+                    <h3>¿Cómo funcionamos?</h3>
+                    <p>
+                        Da click a nuestro video y en sólo 60 segundos conoce <span>Devsafio</span> y cómo estamos <span>cambiando el mundo TI</span>, acelerando y potenciando las células tecnológicas.
+                    </p>
+                </div>
+                <div>
+                    <video src="">
+
+                    </video>
+                </div>
+            </section>
+
+            <section id="sin-juniors-no-hay-seniors">
+                <div>
+                    <h3>Sin Juniors no hay Seniors</h3>
+                    <p>
+                        En este programa unimos la <span>rapidez, eficiencia y visión de un desarrollador junior</span> junto con la <span>experiencia y liderazgo de un senior TI</span>, quien acompañará al talento junior contratado para potenciar tu área digital, acá te explicamos cómo:
+                    </p>
+                </div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                        <h4>Aceleramos</h4>
+                        <p>
+                            Aceleramos el proceso de contratación. Una vez que nos envíes tu necesidad de talento digital recibirás un listado con los mejores candidatos en 5 días.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <h4>Acompañamos</h4>
+                        <p>
+                            Nosotros nos hacemos cargo, tú creces. Todos los desarrolladores que contrates serán guiados y apoyados a nivel técnico por un mentor senior TI experto en tu área sin ningún costo ($0).
+                        </p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <h4>Potenciamos</h4>
+                        <p>
+                            Potenciamos y retenemos el talento. Realizamos un seguimiento de alta calidad para potenciar el crecimiento y desarrollo técnico de todos los desarrolladores contratados.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Section2
