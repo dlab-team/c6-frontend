29e6a422c2bccd396a15fa9a4a4083af17f64de8
ramiroguillen <ramirojguillen@gmail.com>
2022-12-27 16:46:42 -0300

    imagenes y responsive ready

--- src/pages/Home/components/Section2.jsx
+++ src/pages/Home/components/Section2.jsx
@@ -1,18 +1,27 @@
 import React from "react"
+import img1 from "../../../assets/images/pablita-girls-holding-hourglass 1.png"
+import img2 from '../../../assets/images/pablita-636 1.png'
+import img3 from '../../../assets/images/pablita-people-holding-pieces-of-puzzle 1.png'
 
 const Section2 = () => {
     return (
         <>
             <section id="como-funcionamos">
-                <div className="flex container mx-auto">
-                    <div className="w-1/2 m-auto">
-                        <h3 className="text-black font-bold text-3xl mb-10 text-center">¿Cómo funcionamos?</h3>
+                <div className="md:flex container mx-auto">
+                    <div className="m-auto">
+                        <h3 className="text-black font-bold text-3xl mb-10 text-center mt-10 md:mt-0">¿Cómo funcionamos?</h3>
                         <p className="text-black text-2xl text-center">
                             Da click a nuestro video y en sólo 60 segundos conoce <span className="text-primary font-bold">Devsafio</span> y cómo estamos <span className="text-primary font-bold">cambiando el mundo TI</span>, acelerando y potenciando las células tecnológicas.
                         </p>
                     </div>
-                    <div className="w-1/2 m-auto p-3">
-                        <iframe src="https://www.youtube.com/watch?v=dQw4w9WgXcQ" width="556" height="340" loading="lazy" />
+                    <div className="m-auto md:p-3">
+                        <iframe
+                            src="https://youtube.com/embed/z5NPi7WrViU"
+                            className="w-[25rem] h-[14rem] md:w-[38rem] md:h-[24rem]"
+                            loading="lazy"
+                            allow="autoplay; encrypted-media"
+                            allowfullscreen
+                        />
                     </div>
                 </div>
             </section>
@@ -25,25 +34,25 @@ const Section2 = () => {
                     </p>
                 </div>
 
-                <div className="flex container mx-auto mt-16 pb-16 justify-around">
-                    <div className="flex flex-col gap-10 text-center items-center max-w-xs">
-                        <img src="http://placekitten.com/256/144" alt="aceleramos" />
+                <div className="flex flex-col md:flex-row container mx-auto mt-16 pb-16 md:justify-around items-center">
+                    <div className="flex flex-col gap-10 text-center items-center max-w-xs px-6">
+                        <img src={img1} alt="aceleramos" className="pt-4" />
                         <h4 className="text-black font-bold text-2xl">Aceleramos</h4>
                         <p className="text-black text-xl">
                             <span className="font-bold">Aceleramos el proceso de contratación</span>. Una vez que nos envíes tu necesidad de talento digital recibirás un listado con los mejores candidatos en <span className="font-bold">5 días</span>.
                         </p>
                     </div>
 
-                    <div className="flex flex-col gap-10 text-center items-center max-w-xs">
-                        <img src="http://placekitten.com/256/144" alt="acompañamos" />
+                    <div className="flex flex-col gap-10 text-center items-center max-w-xs px-6">
+                        <img src={img2} alt="acompañamos" />
                         <h4 className="text-black font-bold text-2xl">Acompañamos</h4>
                         <p className="text-black text-xl">
                             Nosotros nos hacemos cargo, tú creces. Todos los desarrolladores que contrates <span className="font-bold">serán guiados y apoyados a nivel técnico</span> por un mentor senior TI experto en tu área <span className="font-bold">sin ningún costo ($0)</span>.
                         </p>
                     </div>
 
-                    <div className="flex flex-col gap-10 text-center items-center max-w-xs">
-                        <img src="http://placekitten.com/256/144" alt="potenciamos" />
+                    <div className="flex flex-col gap-10 text-center items-center max-w-xs px-6">
+                        <img src={img3} alt="potenciamos" className="pt-4" />
                         <h4 className="text-black font-bold text-2xl">Potenciamos</h4>
                         <p className="text-black text-xl">
                             Potenciamos y retenemos el talento. Realizamos un <span className="font-bold">seguimiento</span> de alta calidad para <span className="font-bold">potenciar el crecimiento y desarrollo técnico</span> de todos los desarrolladores contratados.
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import 'animate.css';
 import hero from '../../assets/images/referencia.png';
+import Section2 from '../Home/components/Section2';
 
 const IndexPage = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div className="text-center">Hola FrontEnd</div>
-          <div className="hero">
-            <img src={hero} alt="hero" />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Section2/>
+    // <div className="container">
+    //   <div className="row">
+    //     <div className="col-12">
+    //       <div className="text-center">Hola FrontEnd</div>
+    //       <div className="hero">
+    //         <img src={hero} alt="hero" />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
