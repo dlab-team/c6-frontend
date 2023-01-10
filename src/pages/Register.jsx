f955b2af6d7b4fd7d30f37f6744ba69f7d5f16a6
anthony <anthonysa0813@gmail.com>
2023-01-10 11:33:36 -0500

    added the logical to register a new user

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,55 +1,152 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import { ModalCustomMessage } from "../components/modal";
 import ModalLoginRegister from "../components/modal/ModalLoginRegister";
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 export const Register = () => {
+  const [showModalMessage, setShowModalMessage] = useState(false);
+  const [statusCode, setStatusCode] = useState(0);
+  const [messageStatus, setMessageStatus] = useState("");
+
+  const { errors, touched, getFieldProps, values, handleSubmit } = useFormik({
+    initialValues: {
+      name: "",
+      surnames: "",
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+    onSubmit: async (values) => {
+      // setIsLoading(true);
+      console.log(values);
+      const { email, name, password } = values;
+      const response = await fetch(`http://localhost:3000/api/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, name, password }),
+      });
+      const data = await response.json();
+      console.log(data);
+      setShowModalMessage(true);
+      setStatusCode(data.statusCode);
+      setMessageStatus(data.message);
+      setTimeout(() => {
+        setShowModalMessage(false);
+      }, 4000);
+      return data;
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Requerido"),
+      surnames: Yup.string().required("Requerido"),
+      email: Yup.string().email("Debe de ser un email").required("Requerido"),
+      password: Yup.string().required("Requerido"),
+      repeatPassword: Yup.string()
+        .required("Requerido")
+        .oneOf([Yup.ref("password"), null], "Las contraseñas no son iguales"),
+    }),
+  });
+
   return (
     <>
+      {showModalMessage && (
+        <ModalCustomMessage
+          message={messageStatus}
+          type={statusCode >= 200 ? "success" : "error"}
+        />
+      )}
       <Header />
       <ModalLoginRegister>
         <h1 className="text-5xl text-center mb-11">Regístrate</h1>
-        <form className="flex flex-col gap-y-6">
+        <form className="flex flex-col gap-y-6" onSubmit={handleSubmit}>
           <div className="flex gap-5 ">
             <div className="flex items-center gap-3">
               <label>Nombre: </label>
-              <div className="">
+              <div className="flex flex-col">
                 <input
                   type="text"
                   className="px-3 py-1 border rounded-md border-stone-500 bg-sky-100"
+                  {...getFieldProps("name")}
                 />
+                {errors.name && touched.name && (
+                  <span className="text-danger text-red-500">
+                    {errors.name}{" "}
+                  </span>
+                )}
               </div>
             </div>
             <div className="flex items-center gap-3">
               <label>Apellido: </label>
-              <div className="">
+              <div className="flex flex-col">
                 <input
                   type="text"
                   className="px-3 py-1 border rounded-md border-stone-500 bg-sky-100"
+                  {...getFieldProps("surnames")}
                 />
+                {errors.surnames && touched.surnames && (
+                  <span className="text-danger text-red-500">
+                    {errors.surnames}{" "}
+                  </span>
+                )}
               </div>
             </div>
           </div>
           <div className="flex items-center ">
             <span className="block grow">Ingresa tu correo:</span>
-            <input
-              type="email"
-              className="w-full px-3 py-1 border rounded-md border-stone-500 bg-sky-100"
-            />
+            <div className="flex flex-col w-full">
+              <input
+                type="email"
+                className="w-full px-3 py-1 border rounded-md border-stone-500 bg-sky-100"
+                {...getFieldProps("email")}
+              />
+              {errors.email && touched.email && (
+                <span className="text-danger text-red-500">
+                  {errors.email}{" "}
+                </span>
+              )}
+            </div>
           </div>
           <div className="flex items-center ">
             <span className="block grow">Crea tu contraseña:</span>
-            <input
-              type="email"
-              className="w-full px-3 py-1 border rounded-md border-stone-500 bg-sky-100"
-            />
+            <div className="flex flex-col w-full">
+              <input
+                type="password"
+                className="w-full px-3 py-1 border rounded-md border-stone-500 bg-sky-100"
+                {...getFieldProps("password")}
+              />
+              {errors.password && touched.password && (
+                <span className="text-danger text-red-500">
+                  {errors.password}{" "}
+                </span>
+              )}
+            </div>
           </div>{" "}
           <div className="flex items-center ">
-            <span className="block grow">Crea tu contraseña::</span>
-            <input
-              type="email"
-              className="w-full px-3 py-1 border rounded-md border-stone-500 bg-sky-100"
-            />
+            <span className="block grow">Crea tu contraseña:</span>
+            <div className="flex flex-col w-full">
+              <input
+                type="password"
+                className="w-full px-3 py-1 border rounded-md border-stone-500 bg-sky-100"
+                {...getFieldProps("repeatPassword")}
+              />
+              {errors.repeatPassword && touched.repeatPassword && (
+                <span className="text-danger text-red-500">
+                  {errors.repeatPassword}{" "}
+                </span>
+              )}
+            </div>
+          </div>
+          <div className="flex">
+            <button
+              type="submit"
+              className="py-2 px-3 rounded-md bg-blue-600 text-white"
+            >
+              registrarse
+            </button>
           </div>
         </form>
       </ModalLoginRegister>
