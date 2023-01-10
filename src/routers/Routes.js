64d34678f2b3ecc808acebd46c64cc9301e86649
anthony <anthonysa0813@gmail.com>
2023-01-09 22:54:43 -0500

    init register section

--- src/routers/Routes.js
+++ src/routers/Routes.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import { Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
 
 //Puse nombres de las paginas que van a ser usadas en las rutas, pero aun no existen en el index
 // las puse momentaneamente hasta que tengamos los archivos correspodienres en el index
-import { Home, Dashboard, Login, Register, Recovery } from '../pages/index';
+import { Home, Dashboard, Login, Register, Recovery } from "../pages/index";
 
 function RoutesApp() {
   return (
