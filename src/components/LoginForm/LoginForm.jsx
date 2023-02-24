33abad4a7a7194db7c8808a6fe1dfa7232c9bef5
ramiroguillen <ramirojguillen@gmail.com>
2023-02-24 16:42:15 -0300

    dropdown para el usuario, con links y botones funcionando. Faltaria un endpoint del backend para poder solicitar la informacion del usuario y cargarla desde el back

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -91,17 +91,17 @@ const LoginForm = ({ setOpenModal }) => {
             )}
           </div>
 
-          <div class='flex items-center mb-4'>
+          <div className='flex items-center mb-4'>
             <input
               onClick={handleShowPassword}
               id='default-checkbox'
               type='checkbox'
               value=''
-              class='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+              className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
             />
             <label
-              for='default-checkbox'
-              class='ml-2 text-sm font-medium text-gray-900 dark:text-gray-900'
+              htmlFor='default-checkbox'
+              className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-900'
             >
               Mostrar contraseña
             </label>
@@ -113,7 +113,7 @@ const LoginForm = ({ setOpenModal }) => {
             >
               Iniciar Sesión
             </button>
-            {respAuth && <div class='static mt-5 mb-5'>{messageAuth}</div>}
+            {respAuth && <div className='static mt-5 mb-5'>{messageAuth}</div>}
           </div>
           <div className='mt-1 -mb-20'>
             <hr className='border-2 h-1' />
